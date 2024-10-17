18b7998435e6e3eda8c0b7a347d62ee9ab43e9d0
hahnemilym <ehahn481@gmail.com>
2024-10-17 00:31:00 -0500

    remote change

--- rearc_prod/create_env.zsh
+++
@@ -1,6 +0,0 @@
-python3 -m pip install --upgrade pip
-python3 -m venv rearc_project
-source rearc_project/bin/activate
-zip -r bls-lambda-p1-p2-sample-rearc.zip bls-lambda-p1-p2-sample-rearc.py
-zip -r bls-lambda-p3-sample-rearc.zip bls-lambda-p3-sample-rearc.py
-python install_pkgs.py
